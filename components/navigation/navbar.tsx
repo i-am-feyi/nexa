f62200dc1f3d8121e82f1c397514e8ec77514b29
Feyisayo Badmus <feymus99@gmail.com>
2025-02-21 00:06:36 -0500

    Initial Commit: Starter Template ✅

---
+++ components/navigation/navbar.tsx
@@ -0,0 +1,12 @@
+import { div, header } from "framer-motion/client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Icons from "../global/icons";
+import { buttonVariants } from "../ui/button";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
